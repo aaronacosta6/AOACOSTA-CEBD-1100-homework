579cea8e2f12c4a253722a12919ec5810e054002
Aaron O. Acosta <aaronacosta6@gmail.com>
2019-10-22 19:02:38 -0400

    Added Travis
    Added Travis

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -5,9 +5,7 @@ def test_average():
     assert(avg == 5.45)
 
 def test_max(): #checks if we get some answers we're expecting, i.e. the max inflammation for certain patients
-    arth = analysis.max_inflammations(0)
-    assert(arth(12) == 17)
-    assert(arth(45) == 16)
+    assert(analysis.max_inflammations(12) == 17)
 
 def test_acute_patient(): #checks that the acute patient (46) indeed has an average above other patients
     assert(analysis.average_inflammations(46) > analysis.average_inflammations(5))
