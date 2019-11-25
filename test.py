330fef6f8a60e80ee118fcd6fb9e034ca3a286de
Aaron O. Acosta <aaronacosta6@gmail.com>
2019-11-25 17:44:05 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+from aoacluster.aoacluster import cluster
+
+#PLOT 1
+cluster.cluster('/Users/Aaron/Desktop/data/data-1.csv', -10, -4.25, -6.3, -8.6, -1.14, 4.23)
+
+#PLOT 2
+cluster.cluster('/Users/Aaron/Desktop/data/data-2.csv', 21.39, 16.31, 20.93, 1.8, 28.06, 21.73)
+
+#PLOT 3
+cluster.cluster('/Users/Aaron/Desktop/data/data-3.csv', 95.34, 124.41, 136.22, 154.90, 185.33, 185.39)
